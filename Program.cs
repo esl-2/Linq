8e6b7e2ca46aee4a46596d423aa4683dbc617307
esl-2 <eslm4732@gmail.com>
2026-07-10 17:25:12 +0300

    Initial commit for LINQ project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var myemployee = Repository.LoadEmployees();
+        var itemployees = myemployee.Where(e=>e.Gender=="female" &&e.Salary==3000);
+        foreach (var item in itemployees)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    
+}
+
